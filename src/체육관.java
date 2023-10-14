5f49c9c75db699fe59bfba850bb8625fa751c62c
LMH <windmi95@nate.com>
2023-10-14 22:37:40 +0900

    method_update5-7

--- src/체육관.java
+++ src/체육관.java
@@ -3,17 +3,13 @@ import java.util.Scanner;
 public class 체육관 {
     int 바벨_무게;
     Scanner scanner = new Scanner(System.in);
-    캐릭터 등급_무게_구별 = new 캐릭터();
-
-    public void 바벨() {//중량 1~ 10 -> 초급 중량 1 ~ 50 -> 중급 중량
-        if (등급_무게_구별.등급 == 1) {
-            System.out.println("중량 1 ~ 10kg");
-        } else if (등급_무게_구별.등급 == 2) {
-            System.out.println("중량 1 ~ 50kg");
-        } else if (등급_무게_구별.등급 == 3) {
-            System.out.println("중량 1 ~ 100kg");
+    캐릭터 캐릭터_선택할_수_있는_무게체크 = new 캐릭터();
+    public boolean 선택할_수_있는_무게체크(캐릭터_중량, 캐릭터_레벨) {//중량 1~ 10 -> 초급 중량 1 ~ 50 -> 중급 중량
+        if (캐릭터_선택할_수_있는_무게체크.레벨 < 10 && 캐릭터_선택할_수_있는_무게체크.레벨 > 10) {
+            return false;
+        } else {
+            return true;
         }
-
     }
-
 }
+
