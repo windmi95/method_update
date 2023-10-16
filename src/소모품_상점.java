82ab446e91fd20f3d1e720a1f24150e562f0da5a
leedonghun <dlwldlwldus123@gmail.com>
2023-10-16 15:59:32 +0900

    sample -리만희 뻐큐

--- src/소모품_상점.java
+++ src/소모품_상점.java
@@ -8,4 +8,41 @@ public class 소모품_상점 {
 
     int 크레아틴 = 700;
 
+    public void 소모품_목록() {
+        System.out.println("1.bcaa 2.탄수화물 보충제 3.프로틴 4.크레아틴");
+        System.out.println("어떤 것을 구매하시겠습니까?");
+    }
+
+    public void 소모품_개수체크(){
+        System.out.println("몇개나 구매하시겠습니까?");
+    }
+
+
+    public void 소모품_구매(int 소모품_구매_선택_번호,int 소모품_구매_개수,캐릭터 내캐릭터,가방 내가방) {
+        if (소모품_구매_선택_번호 == 1) {
+            System.out.println("현재 소지하고 있는 골드 -> " + 내캐릭터.보유한_골드);
+            int 총금액 = this.bcaa * 소모품_구매_개수;
+            if (내캐릭터.보유한_골드 >= 총금액) {//살수 있다.
+                //구매햇으니까  차감해준다.
+                내캐릭터.보유한_골드 = 내캐릭터.보유한_골드 - this.bcaa * 소모품_구매_개수;
+                System.out.println("현재 소지하고 있는 골드 -> " + 내캐릭터.보유한_골드);
+            } else {//살수 없을때
+                System.out.println("골드가 부족합니다.");
+            }
+        } else if (소모품_구매_선택_번호 == 2) {
+            System.out.println("현재 소지하고 있는 골드 -> " + 내캐릭터.보유한_골드);
+            System.out.println("탄수화물 보충제를 선택하셨습니다.");
+            System.out.println("몇 개를 구매하시겠습니까?");
+            int 총금액 = this.탄수화물_보충제 * 소모품_구매_개수;
+            if (내캐릭터.보유한_골드 >= 총금액) {
+                내캐릭터.보유한_골드 = 내캐릭터.보유한_골드 - this.탄수화물_보충제 * 소모품_구매_개수;
+                System.out.println("현재 소지하고 있는 골드 -> " + 내캐릭터.보유한_골드);
+            } else {
+                System.out.println("골드가 부족합니다.");
+            }
+        } else{
+            System.out.println("잘못된 번호 입니다. ");
+        }
+    }
 }
+
--- src/체육관.java
+++ src/체육관.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-
+//중량을 선택할 수 있는 클래스
 public class 체육관 {
     int 바벨_무게;
-    Scanner scanner = new Scanner(System.in);
-    캐릭터 게임_캐릭터 = new 캐릭터();
+//    Scanner scanner = new Scanner(System.in);
+//    캐릭터 게임_캐릭터 = new 캐릭터();
 
     public boolean 선택할_수_있는_무게체크(int 캐릭터_중량, int 캐릭터_레벨) {
-        if (게임_캐릭터.레벨 < 10 && 게임_캐릭터.레벨 > 10) {
+        if (캐릭터_레벨< 10 && 캐릭터_중량 > 10) {
             System.out.println("해당 중량은 사용할 수 없습니다.");
             return false;
         } else {
