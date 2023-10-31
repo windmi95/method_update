86f732f865b450caeb1e68a63c7b6776e182b729
LMH <windmi95@nate.com>
2023-11-01 00:54:52 +0900

    method update 8-9

--- src/캐릭터.java
+++ src/캐릭터.java
@@ -4,14 +4,14 @@ public class 캐릭터 {
     int 등급 = 1;
     int 경험치양 = 0;
     int 보유한_골드 = 5000;
-    int 레벨 = 100;
+    int 레벨 = 1;
     int 중량_선택 = 0;
     int 스탯_포인트 = 5;
     private 손목보호대 손목보호대 = null;
     private 리프팅웨어 리프팅웨어 = null;
     private 역도화 역도화 = null;
     private 복압_벨트 복압벨트 = null;
-    boolean 캐릭터_장착_메뉴_확인중 = true;
+    boolean 캐릭터_메뉴_확인중 = true;
 
     public void 손목보호대_장착(손목보호대 손목보호대, 가방 내가방) {
         this.손목보호대 = 손목보호대;
@@ -60,9 +60,44 @@ public class 캐릭터 {
             System.out.println("복압벨트");
         } else {
             System.out.println("아무것도 착용중인 장비가 없습니다.");
-            캐릭터_장착_메뉴_확인중 = false;
+            캐릭터_메뉴_확인중 = false;
         }
     }
+    public int 캐릭터_해제할_장비_선택(int 해제할_장비_번호_선택) {
+        if (해제할_장비_번호_선택 == 1) {
+            System.out.println("손목보호대를 장착하시겠습니까?");
+            System.out.println("1.장착 해제한다 2.장착 해제하지 않는다.");
+        } else if (해제할_장비_번호_선택 == 2) {
+            System.out.println("리프팅웨어를 장착하시겠습니까?");
+            System.out.println("1.장착 해제한다 2.장착 해제하지 않는다.");
+        } else if (해제할_장비_번호_선택 == 3) {
+            System.out.println("역도화를 장착하시겠습니까?");
+            System.out.println("1.장착 해제한다 2.장착 해제하지 않는다.");
+        } else if (해제할_장비_번호_선택 == 4) {
+            System.out.println("복압벨트를 장착하시겠습니까?");
+            System.out.println("1.장착 해제한다 2.장착 해제하지 않는다.");
+        } else {
+            System.out.println("잘 못 입력하셨습니다.");
+        }
+        return 해제할_장비_번호_선택;
+    }
+
+
+    public void 캐릭터_장비_해제할_수_있는_목록_출력() {
+        if (this.손목보호대 != null && this.손목보호대.캐릭터한테_장착되어있는지_여부) {
+            System.out.println("1.손목보호대");
+        } else if (this.리프팅웨어 != null && this.리프팅웨어.캐릭터한테_장착되어있는지_여부){
+            System.out.println("2.리프팅웨어");
+        } else if (this.역도화 != null && this.역도화.캐릭터한테_장착되어있는지_여부) {
+            System.out.println("3.역도화");
+        } else if (this.복압벨트 != null && this.복압벨트.캐릭터한테_장착되어있는지_여부) {
+            System.out.println("4.복압벨트");
+        } else {
+            System.out.println("아무것도 착용중인 장비가 없습니다.");
+            캐릭터_메뉴_확인중 = false;
+        }
+    }
+
     public void 캐릭터_스탯() {
         System.out.println("현재 체력 => " + 체력);
         System.out.println("현재 근력 => " + 근력);
--- src/플레이.java
+++ src/플레이.java
@@ -71,14 +71,14 @@ public class 플레이 {
                     if (가방_메뉴_선택_번호 == 1) {//장비 선택을 골랐을 때
                         가방.장비상점에서_구매한_장비_목록보여주기();
                         int 장착할_장비_번호_선택 = 가방.장착할_장비_번호_선택(scanner.nextInt());
-                        if (가방.손목보호대 != null && 장착할_장비_번호_선택  == 1) {
-                            int 장착_선택_여부_선택 = scanner.nextInt();
-                            if (장착_선택_여부_선택 == 1) {
+                        if (가방.손목보호대 != null && 장착할_장비_번호_선택 == 1) {
+                            int 장비_장착_선택_여부_선택 = scanner.nextInt();
+                            if (장비_장착_선택_여부_선택 == 1) {
                                 System.out.println("손목보호대를 장착하겠습니다.");
                                 게임_캐릭터.손목보호대_장착(가방.손목보호대, 가방);
                                 가방.손목보호대 = null;
                                 break;
-                            } else if (장착_선택_여부_선택 == 2) {
+                            } else if (장비_장착_선택_여부_선택 == 2) {
                                 System.out.println("가방 메뉴로 이동하겠습니다.");
                                 break;
                             } else {
@@ -86,13 +86,13 @@ public class 플레이 {
                                 continue;
                             }
                         } else if (가방.리프팅웨어 != null && 장착할_장비_번호_선택 == 2) {
-                            int 장착_선택_여부_선택 = scanner.nextInt();
-                            if (장착_선택_여부_선택 == 1) {
+                            int 장비_장착_선택_여부_선택 = scanner.nextInt();
+                            if (장비_장착_선택_여부_선택 == 1) {
                                 System.out.println("리프팅웨어를 장착하겠습니다.");
                                 게임_캐릭터.리프팅웨어_장착(가방.리프팅웨어, 가방);
                                 가방.리프팅웨어 = null;
                                 break;
-                            } else if (장착_선택_여부_선택 == 2) {
+                            } else if (장비_장착_선택_여부_선택 == 2) {
                                 System.out.println("가방 메뉴로 이동하겠습니다.");
                                 break;
                             } else {
@@ -100,13 +100,13 @@ public class 플레이 {
                                 continue;
                             }
                         } else if (가방.역도화 != null && 장착할_장비_번호_선택 == 3) {
-                            int 장착_선택_여부_선택 = scanner.nextInt();
-                            if (장착_선택_여부_선택 == 1) {
+                            int 장비_장착_선택_여부_선택 = scanner.nextInt();
+                            if (장비_장착_선택_여부_선택 == 1) {
                                 System.out.println("역도화를 장착하겠습니다.");
                                 게임_캐릭터.역도화_장착(가방.역도화, 가방);
                                 가방.역도화 = null;
                                 break;
-                            } else if (장착_선택_여부_선택 == 2) {
+                            } else if (장비_장착_선택_여부_선택 == 2) {
                                 System.out.println("가방 메뉴로 이동하겠습니다.");
                                 break;
                             } else {
@@ -114,13 +114,13 @@ public class 플레이 {
                                 continue;
                             }
                         } else if (가방.복압_벨트 != null && 장착할_장비_번호_선택  == 4) {
-                            int 장착_선택_여부_선택 = scanner.nextInt();
-                            if (장착_선택_여부_선택 == 1) {
+                            int 장비_장착_선택_여부_선택 = scanner.nextInt();
+                            if (장비_장착_선택_여부_선택 == 1) {
                                 System.out.println("복압벨트를 장착하겠습니다.");
                                 게임_캐릭터.복압벨트_장착(가방.복압_벨트, 가방);
                                 가방.복압_벨트 = null;
                                 break;
-                            } else if (장착_선택_여부_선택 == 2) {
+                            } else if (장비_장착_선택_여부_선택 == 2) {
                                 System.out.println("가방 메뉴로 이동하겠습니다.");
                                 break;
                             } else {
@@ -132,10 +132,55 @@ public class 플레이 {
                             break;
                         }
                     } else if (가방_메뉴_선택_번호 == 2) {
-                        게임_캐릭터.손목보호대_벗기();
-                        게임_캐릭터.리프팅웨어_벗기();
-                        게임_캐릭터.역도화_벗기();
-                        게임_캐릭터.복압벨트_벗기();
+                        while(가방.가방_메뉴_진행중) {
+                            게임_캐릭터.캐릭터_장비_해제할_수_있는_목록_출력();
+                            int 해제할_장비_번호_선택 = 게임_캐릭터.캐릭터_해제할_장비_선택(scanner.nextInt());
+                            if (해제할_장비_번호_선택 == 1) {
+                                int 장비_해제_여부_선택 = scanner.nextInt();
+                                if (장비_해제_여부_선택 == 1) {
+                                    System.out.println("손목보호대 장착을 해제하겠습니다.");
+                                    게임_캐릭터.손목보호대_벗기();
+                                } else if (장비_해제_여부_선택 == 2) {
+                                    System.out.println("가방 메뉴로 이동하겠습니다.");
+                                } else {
+                                    System.out.println("잘 못 입력하셨습니다.");
+                                    continue;
+                                }
+                            } else if (해제할_장비_번호_선택 == 2) {
+                                int 장비_해제_여부_선택 = scanner.nextInt();
+                                if (장비_해제_여부_선택 == 1) {
+                                    System.out.println("리프팅웨어 장착을 해제하겠습니다.");
+                                    게임_캐릭터.리프팅웨어_벗기();
+                                } else if (장비_해제_여부_선택 == 2) {
+                                    System.out.println("가방 메뉴로 이동하겠습니다.");
+                                } else {
+                                    System.out.println("잘 못 입력하셨습니다.");
+                                    continue;
+                                }
+                            } else if (해제할_장비_번호_선택 == 3) {
+                                int 장비_해제_여부_선택 = scanner.nextInt();
+                                if (장비_해제_여부_선택 == 1) {
+                                    System.out.println("역도화 장착을 해제하겠습니다.");
+                                    게임_캐릭터.역도화_벗기();
+                                } else if (장비_해제_여부_선택 == 2) {
+                                    System.out.println("가방 메뉴로 이동하겠습니다.");
+                                } else {
+                                    System.out.println("잘 못 입력하셨습니다.");
+                                    continue;
+                                }
+                            } else if (해제할_장비_번호_선택 == 4) {
+                                int 장비_해제_여부_선택 = scanner.nextInt();
+                                if (장비_해제_여부_선택 == 1) {
+                                    System.out.println("복압벨트 장착을 해제하겠습니다.");
+                                    게임_캐릭터.복압벨트_벗기();
+                                } else if (장비_해제_여부_선택 == 2) {
+                                    System.out.println("가방 메뉴로 이동하겠습니다.");
+                                } else {
+                                    System.out.println("잘 못 입력하셨습니다.");
+                                    continue;
+                                }
+                            }
+                        }
                     } else if (가방_메뉴_선택_번호 == 3) {
                         게임_캐릭터.캐릭터_장착한_장비_리스트_보여주기();
                         break;
